18a104d88259a710f72df3a3077e6f6d019a07ee
zetsuboii <hmzkrb@gmail.com>
2024-01-30 19:28:09 +0300

    feat: add Sparse.sol and its tests

---
+++ src/Sparse.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Blake2s} from "./Blake2s.sol";
+import "forge-std/console.sol";
+
+// Type conversion between Rust implementation and Solidity implementation
+// H32          :           bytes32
+// U256         :           uint256
+// Key          :           uint256
+// ValueHash    :           bytes32
+
+/// @title Sparse Tree Entry
+/// @member value The value of the entry
+/// @member leafIndex The index of the leaf in the tree
+struct TreeEntry {
+    uint256 key;
+    bytes32 value;
+    uint64 leafIndex;
+}
+
+uint256 constant KEY_SIZE = 32;
+uint256 constant TREE_DEPTH = KEY_SIZE * 8;
+
+contract Sparse {
+    mapping(uint256 => TreeEntry) public tree;
+
+    /// @notice Folds the merkle tree
+    function foldMerklePath(
+        bytes32[] memory path,
+        TreeEntry memory entry
+    ) public pure returns (bytes32) {
+        bytes32 hashValue = hashLeaf(entry.leafIndex, entry.value);
+        bytes32[] memory full_path = extendMerklePath(path);
+        for (uint256 depth = 0; depth < full_path.length; depth++) {
+            bytes32 adjacentHash = full_path[depth];
+            if (bit(entry.key, depth)) {
+                hashValue = hashBranch(adjacentHash, hashValue);
+            } else {
+                hashValue = hashBranch(hashValue, adjacentHash);
+            }
+        }
+        return hashValue;
+    }
+
+    function hashBranch(
+        bytes32 left,
+        bytes32 right
+    ) public pure returns (bytes32) {
+        bytes32 res = compress(left, right);
+        return res;
+    }
+
+    function compress(
+        bytes32 left,
+        bytes32 right
+    ) public pure returns (bytes32 result) {
+        uint32[8] memory digest = Blake2s.toDigest(abi.encodePacked(left), abi.encodePacked(right));
+
+        // Loop through each 32-bit word in the digest array and construct a single bytes32 result.
+        // This is done by shifting each 32-bit word to its correct position in the 256-bit result
+        // and combining them using the bitwise OR operation.
+        for (uint i = 0; i < digest.length; i++) {
+            result = bytes32(
+                uint256(result) | (uint256(digest[i]) << (256 - ((i + 1) * 32)))
+            );
+        }
+    }
+
+
+    function emptyLeaf() public pure returns (bytes memory) {
+        return new bytes(40);
+    }
+
+    /// @notice Hashes an individual leaf
+    function hashLeaf(
+        uint64 leafIndex,
+        bytes32 value
+    ) public pure returns (bytes32) {
+        bytes memory input = new bytes(40);
+        assembly {
+            // Store leafIndex at first 8 bytes
+            mstore(add(input, 0x20), shl(192, leafIndex))
+            // Store value at last 32 bytes
+            mstore(add(input, 0x28), value)
+        }
+        return Blake2s.toBytes32(input);
+    }
+
+    function extendMerklePath(
+        bytes32[] memory path
+    ) public pure returns (bytes32[] memory) {
+        uint256 emptyHashCount = TREE_DEPTH - path.length;
+        bytes32[] memory hashes = new bytes32[](emptyHashCount + path.length);
+        for (uint256 i = 0; i < emptyHashCount; i++) {
+            hashes[i] = emptySubtreeHash(i);
+        }
+        for (uint256 i = 0; i < path.length; i++) {
+            hashes[emptyHashCount + i] = path[i];
+        }
+        return hashes;
+    }
+
+    function emptySubtreeHash(uint256 depth) public pure returns (bytes32) {
+        bytes32[] memory emptyTreeHashes = computeEmptyTreeHashes();
+        return emptyTreeHashes[depth];
+    }
+
+    function computeEmptyTreeHashes() public pure returns (bytes32[] memory) {
+        bytes32[] memory emptyTreeHashes = new bytes32[](TREE_DEPTH + 1);
+        emptyTreeHashes[0] = Blake2s.toBytes32(emptyLeaf());
+
+        bytes32 lastHash = emptyTreeHashes[0];
+        for (uint256 i = 1; i < TREE_DEPTH + 1; i++) {
+            lastHash = Blake2s.toBytes32(abi.encodePacked(lastHash, lastHash));
+            emptyTreeHashes[i] = lastHash;
+        }
+        return emptyTreeHashes;
+    }
+
+    function bit(
+        uint256 value,
+        uint256 bitOffset
+    ) public pure returns (bool) {
+        return (value >> bitOffset) & 1 == 1;
+    }
+}
---
+++ test/Sparse.t.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {Sparse, TreeEntry, TREE_DEPTH} from "../src/Sparse.sol";
+
+contract SparseTest is Test {
+    Sparse public sparse;
+
+    function setUp() public {
+        sparse = new Sparse();
+    }
+
+    function test_emptyTreeHash() public {
+        bytes32 hash = sparse.emptySubtreeHash(TREE_DEPTH);
+        assertEq(
+            hash,
+            bytes32(
+                0x98a48e4ed1736188384ae8a79dd21c4d6687e5fd22ca18148906d78736c0d86a
+            )
+        );
+    }
+
+    function test_hashLeaf() public {
+        TreeEntry memory entry = TreeEntry({
+            key: 42366496704336254375416776462386343662429697233927452559041593589277704190797,
+            value: 0x0101010101010101010101010101010101010101010101010101010101010101,
+            leafIndex: 1
+        });
+        bytes32 hashValue = sparse.hashLeaf(entry.leafIndex, entry.value);
+        assertEq(
+            hashValue,
+            bytes32(
+                0x52f28950612240cb4fb218fbf6df83f6dbe271c056900b11966cfb9404c1dce0
+            )
+        );
+    }
+
+    function test_compress() public {
+        bytes32 lhs = bytes32(
+            0x6bbb316d292155ad8d2b47a03504033efbf70074141130e9e346a798f5904921
+        );
+        bytes32 rhs = bytes32(
+            0xc546f1eef1d499b7b6966254ec541653a205ed4bf7ae3f1ee1ddca773c009e85
+        );
+        bytes32 res = sparse.compress(lhs, rhs);
+        assertEq(
+            res,
+            bytes32(
+                0x65d2ecfac89775755ba564e7a72d108cebffcaa1d138b6d89a59cbcbf664bc6a
+            )
+        );
+    }
+
+    function test_extendMerklePath() public {
+        bytes32[] memory path = new bytes32[](0);
+        bytes32[] memory full_path = sparse.extendMerklePath(path);
+        assertEq(full_path.length, 256);
+
+        bytes32 hash0 = 0x94bb15542026f4f607416f019dffe21bb39bbb32cc92085ab615660a6b5fbef4;
+        bytes32 hash1 = 0x7952661ab5d63534c5ea72f81887d8dd6bf514b14c8e9fb714b6feb02efb96a0;
+        bytes32 hash2 = 0x3d75808db532e9685bcc7969ad0f5f0872086b24e02b28cdc7df6e3cc1bd2371;
+        bytes32 hash253 = 0x7f391690461b8e3468e2f6ba0fcba50df0195bd6d1bb187180650b00b2a13d5a;
+        bytes32 hash254 = 0x6bbb316d292155ad8d2b47a03504033efbf70074141130e9e346a798f5904921;
+        bytes32 hash255 = 0x395ebe57b2b0ca2592bc9b173eaaedf722c0121cf908386bf2b56d0179fde9c0;
+
+        // Check that the first 3 and last 3 hashes are correct
+        assertEq(full_path[0], hash0);
+        assertEq(full_path[1], hash1);
+        assertEq(full_path[2], hash2);
+        assertEq(full_path[253], hash253);
+        assertEq(full_path[254], hash254);
+        assertEq(full_path[255], hash255);
+    }
+
+    function test_foldMerklePath() public {
+        TreeEntry memory entry = TreeEntry({
+            key: 42366496704336254375416776462386343662429697233927452559041593589277704190797,
+            value: 0x0101010101010101010101010101010101010101010101010101010101010101,
+            leafIndex: 1
+        });
+        bytes32[] memory path = new bytes32[](0);
+
+        bytes32 hash = sparse.foldMerklePath(path, entry);
+        assertEq(
+            hash,
+            bytes32(
+                0x7f00a6b2eede960857703c8cb9e96f28b910e6693412cea4b006f24239b681e0
+            )
+        );
+    }
+}
